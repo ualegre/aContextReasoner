e6078365f06c92a732bbec632dfad6c62533b834
Dean Kramer <d.kramer@mdx.ac.uk>
2015-03-31 12:05:11 +0100

    Added environment ontology and first time operations
    Best make sure we copy packaged ontologies on the SD card for use, but only the first time the system is run.
    
    Also, changed the position of SOUPA ontologies to be in their own folder.

--- reasoner/src/main/java/org/poseidon_project/context/reasoner/OntologyManager.java
+++ reasoner/src/main/java/org/poseidon_project/context/reasoner/OntologyManager.java
@@ -17,6 +17,8 @@
 package org.poseidon_project.context.reasoner;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Environment;
 import android.util.Log;
 
 import com.hp.hpl.jena.ontology.OntDocumentManager;
@@ -30,11 +32,14 @@ import com.hp.hpl.jena.query.ResultSet;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 
 import org.poseidon_project.context.ContextReasonerCore;
+import org.poseidon_project.contexts.IOntologyManager;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -43,16 +48,79 @@ import java.util.Map;
  *
  * @author Dean Kramer <d.kramer@mdx.ac.uk>
  */
-public class OntologyManager {
+public class OntologyManager implements IOntologyManager{
 
     private Context mContext;
     private OntModel mModel = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM);
     private static final String LOGTAG  = "OntologyManager";
+    private static final String ONTOLOGY_PREFS = "OntologyPrefs";
     private ContextReasonerCore mReasonerCore;
 
     public OntologyManager(Context context, ContextReasonerCore core){
         mContext = context;
         mReasonerCore = core;
+        runFirstTime();
+    }
+
+    private void runFirstTime() {
+        SharedPreferences settings = mContext.getSharedPreferences(ONTOLOGY_PREFS, 0);
+
+        boolean beenRun = settings.getBoolean("ranFirst", false);
+
+        if (! beenRun ) {
+            try {
+                InputStream in = mContext.getAssets().open("ontologyMap.json");
+
+                OntologyFileMapParser parser = new OntologyFileMapParser(in);
+                HashMap<String, String> toBeCopied = parser.parse();
+
+                for(String filepath : toBeCopied.values()) {
+                    boolean copied = copyOntologyFile(filepath);
+
+                    if (! copied ){
+                        Log.e(LOGTAG, "Failed to copy to SD Card: " + filepath);
+                    }
+                }
+
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putBoolean("ranfirst", true);
+
+            } catch (IOException e) {
+                Log.e(LOGTAG, e.getStackTrace().toString());
+            }
+        }
+
+    }
+
+    public boolean copyOntologyFile(String filepath) {
+
+        try {
+            filepath = filepath.replace("$SD$",
+                    Environment.getExternalStorageDirectory().getAbsolutePath());
+            String filename = filepath.substring(filepath.lastIndexOf("/"));
+
+            InputStream in = mContext.getAssets().open(filename);
+            File output = new File(filepath);
+            OutputStream out = new FileOutputStream(output);
+
+            byte[] buffer = new byte[8192];
+            int read;
+            while ((read = in.read(buffer)) != -1)
+            {
+                out.write(buffer, 0, read);
+            }
+            in.close();
+            in = null;
+            out.flush();
+            out.close();
+            out = null;
+
+        } catch (IOException e) {
+            Log.e(LOGTAG, e.getStackTrace().toString());
+            return false;
+        }
+
+        return true;
     }
 
     public void loadOntologyFromFile(OntModel model, String location) {

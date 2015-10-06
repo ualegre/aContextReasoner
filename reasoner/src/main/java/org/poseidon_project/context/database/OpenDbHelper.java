43b0e0b500f3cc655fc1dc561a060fcae36a3aa3
Dean Kramer <d.kramer@mdx.ac.uk>
2015-10-06 13:23:56 +0200

    When upgrading, just recreate tables using new schema

--- reasoner/src/main/java/org/poseidon_project/context/database/OpenDbHelper.java
+++ reasoner/src/main/java/org/poseidon_project/context/database/OpenDbHelper.java
@@ -24,25 +24,24 @@ import android.database.sqlite.SQLiteOpenHelper;
  *
  * @author Dean Kramer <d.kramer@mdx.ac.uk>
  */
-public class OpenDbHelper extends SQLiteOpenHelper{
+public class OpenDbHelper extends SQLiteOpenHelper {
 
-    private static final int DATABASE_VERSION = 1;
-    private static final String DB_NAME = "contextDB";
     public static final String CONTEXTTABLE = "usable_contexts";
+    public static final String RECEIVERTABLE = "usable_receivers";
+    public static final String DEBUGEVENTSTABLE = "events_data";
+    private static final int DATABASE_VERSION = 2;
+    private static final String DB_NAME = "contextDB";
     private static final String CONTEXTTABLE_CREATE = "create table usable_contexts (_id integer primary key autoincrement, "
             + "packagename text,"
             + "name text,"
             + "owner text,"
             + "permission int not null,"
             + "dex_file text);";
-
-    public static final String RECEIVERTABLE = "usable_receivers";
     private static final String RECEIVERTABLE_CREATE = "create table usable_receivers (_id integer primary key autoincrement, "
             + "packagename text,"
             + "name text,"
             + "owner text,"
             + "dex_file text);";
-    public static final String DEBUGEVENTSTABLE = "events_data";
     private static final String DEBUGEVENTSTABLE_CREATE = "create table events_data (_id integer primary key autoincrement, "
             + "eventOrigin smallint,"
             + "eventLocation text,"
@@ -62,7 +61,17 @@ public class OpenDbHelper extends SQLiteOpenHelper{
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
+    public void onUpgrade(SQLiteDatabase db, int curv, int newv) {
+
+        db.execSQL("DROP TABLE IF EXISTS " + CONTEXTTABLE);
+        db.execSQL(CONTEXTTABLE_CREATE);
+        insertStandardContexts(db);
+
+        db.execSQL("DROP TABLE IF EXISTS " + RECEIVERTABLE);
+        db.execSQL(RECEIVERTABLE_CREATE);
+
+        db.execSQL("DROP TABLE IF EXISTS " + DEBUGEVENTSTABLE);
+        db.execSQL(DEBUGEVENTSTABLE_CREATE);
 
     }
 

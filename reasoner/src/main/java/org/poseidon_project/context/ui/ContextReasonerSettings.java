ec5deecf0bc26e146d03a0facb4f331c5ac70538
Dean Kramer <d.kramer@mdx.ac.uk>
2016-02-17 09:28:41 +0000

    Authenticate with Tellu Service for Preference Access.

--- reasoner/src/main/java/org/poseidon_project/context/ui/ContextReasonerSettings.java
+++ reasoner/src/main/java/org/poseidon_project/context/ui/ContextReasonerSettings.java
@@ -17,7 +17,14 @@
 package org.poseidon_project.context.ui;
 
 import android.app.Activity;
+import android.app.FragmentManager;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import org.poseidon_project.context.R;
+
+import no.tellu.findit.client.api.AsyncService;
+import no.tellu.findit.client.api.AsyncServiceImpl;
 
 
 /**
@@ -25,17 +32,65 @@ import android.os.Bundle;
  *
  * @author Dean Kramer <d.kramer@mdx.ac.uk>
  */
-public class ContextReasonerSettings extends Activity {
+public class ContextReasonerSettings extends Activity implements DialogReturnInterface {
 
+    //Tellu related
+    private AsyncService mTelluApiService;
+    private FragmentManager mFragManager;
+    private final static String mTelluApiServiceURL = "https://ri.smarttracker.no/web";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getFragmentManager().beginTransaction()
+        mFragManager = getFragmentManager();
+
+        mFragManager.beginTransaction()
                 .replace(android.R.id.content, new ContextReasonerSettingsFragment())
                 .commit();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        LoginDialogFragment loginFrag = LoginDialogFragment.newInstance(R.string.logintitle);
+        loginFrag.show(mFragManager, "dialog");
+    }
+
+    public void authenticateUser(String username, String password) {
+        mTelluApiService = new AsyncServiceImpl(mTelluApiServiceURL);
+
+        AsyncService.AccountCallback callback = new AsyncService.AccountCallback() {
+            @Override
+            public void onAccountOK(long customerId, String customer) {
+                Toast.makeText(getApplicationContext(), R.string.authyes, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAccountError(int code, String message, Exception e) {
+                Toast.makeText(getApplicationContext(), R.string.authno, Toast.LENGTH_SHORT).show();
+                LoginDialogFragment loginFrag = LoginDialogFragment.newInstance(R.string.logintitle);
+                loginFrag.show(mFragManager, "dialog");
+
+            }
+        };
+
+        mTelluApiService.initiate(username, password, callback);
 
     }
 
+    @Override
+    public void doPositiveButtonClick(Object... para) {
+        authenticateUser((String) para[0], (String) para[1]);
+    }
+
+    @Override
+    public void doNegativeButtonClick(Object... para) {
+        finish();
+    }
+
+    @Override
+    public void doNeutralButtonClick(Object... para) {
+
+    }
 }
